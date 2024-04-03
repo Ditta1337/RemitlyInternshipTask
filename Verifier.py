f879cb4883e7e32648e6e406c3f3270958be21ea
arturdwornik <arturdittadwornik@gmail.com>
2024-04-03 21:29:15 +0200

    Added AWS::IAM::Role Policy format checker

--- Verifier.py
+++ Verifier.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 class Verifier:
     @staticmethod
@@ -11,17 +12,43 @@ class Verifier:
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"Path {file_path} does not exist")
         
-        # check if its a json
+        # check if its a JSON
         try:
             with open(file_path, "r") as file:
                 data = json.load(file)
         except:
             raise ValueError(f"File {file_path} is not a valid JSON file")
         
+        # check if the JSON file is AWS::IAM::Role Policy JSON file
+        if not Verifier.__check_Role_Policy_format(data):
+            raise ValueError(f"File {file_path} is not a valid AWS::IAM::Role Policy JSON file")
+        
         # check if "Resource" field is present in any statement and if so, if it is "*"
         for statement in data["PolicyDocument"]["Statement"]:
             if "Resource" in statement and statement["Resource"] == "*":
                 return False
+        return True
+    
+    @staticmethod
+    def __check_Role_Policy_format(data: dict) -> bool:
+        """
+        Check if the JSON file is AWS::IAM::Role Policy JSON file
+        """
+        # Check if "PolicyName" and "PolicyDocument" keys exist
+        if "PolicyName" not in data or "PolicyDocument" not in data:
+            return False
+
+        # Check if "PolicyDocument" is a JSON
+        if not isinstance(data["PolicyDocument"], dict):
+            return False
+
+        # Check if "PolicyName" is a string
+        if not isinstance(data["PolicyName"], str):
+            return False
+
+        # Check if "PolicyName" matches the pattern and its length is between 1 and 128
+        if not re.match(r'^[\w+=,.@-]+$', data["PolicyName"]) or not (1 <= len(data["PolicyName"]) <= 128):
+            return False
         return True
 
             
--- test_verifier.py
+++ test_verifier.py
@@ -3,7 +3,7 @@ from Verifier import Verifier
 
 class TestVerifier(unittest.TestCase):
     def test_verify_json_resource_asterisk(self):
-        #test1.json has a "Resource" field with single "*"
+        # test1.json has a "Resource" field with single "*"
         self.assertFalse(Verifier.verify_json("./test_examples/test1.json"))
 
     def test_verify_json_resource_other(self):
@@ -16,13 +16,19 @@ class TestVerifier(unittest.TestCase):
             Verifier.verify_json("./test_examples/test3.json")
 
     def test_verify_json_no_resource_field(self):
-        # test3.json does not have a "Resource" field
+        # test4.json does not have a "Resource" field
         self.assertTrue(Verifier.verify_json("./test_examples/test4.json"))
+
+    def test_verify_json_invalid_policy_syntax(self):
+        # test5.json does not have a "PolicyName" field
+        with self.assertRaises(ValueError):
+            Verifier.verify_json("./test_examples/test5.json")
 
     def test_verify_json_file_not_found(self):
         # path_not_existent.json does not exist
         with self.assertRaises(FileNotFoundError):
             Verifier.verify_json("path_not_existent.json")
+
 
 if __name__ == '__main__':
     unittest.main()
